02842471885813a74a6ddb8c7f8b85cac43938d4
lishunhang <42265253+lishunhang@users.noreply.github.com>
2018-12-18 16:39:55 +0800

    Create dataModel.java

---
+++ CART/dataModel.java
@@ -0,0 +1,21 @@
+package com.GBDT.CART;
+
+public class dataModel {
+
+	private String column;
+	private double[] values;
+	public String getColumn() {
+		return column;
+	}
+	public void setColumn(String column) {
+		this.column = column;
+	}
+	public double[] getValues() {
+		return values;
+	}
+	public void setValues(double[] values) {
+		this.values = values;
+	}
+	
+	
+}
